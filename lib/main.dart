05d7e973c9c9758c77390c11cef63e37bf350e46
Mike Dubinsky <mike.dubinsky@odb.org>
2019-08-23 09:48:44 -0400

    Integrated calendar: needs additional configuration

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,8 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:path_provider/path_provider.dart';
-
+import 'package:intl/intl.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'player_widget.dart';
 
 typedef void OnError(Exception exception);
@@ -28,6 +29,10 @@ class _ExampleAppState extends State<ExampleApp> {
   AudioCache audioCache = AudioCache();
   AudioPlayer advancedPlayer = AudioPlayer();
   String localFilePath;
+  String datePicked;
+  DateTime date;
+  TimeOfDay time;
+  final dateFormat = DateFormat("MMMM-dd-yyyy");
 
   Future _loadFile() async {
     final bytes = await readBytes(kUrl1);
@@ -60,66 +65,24 @@ class _ExampleAppState extends State<ExampleApp> {
   Widget remoteUrl() {
     return SingleChildScrollView(
       child: _tab([
-        Text(
-          '8/18 ($kUrl1)',
+        DateTimeForm(),
+                Text(
+         'Date: 8/18\n$kUrl1',
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
         PlayerWidget(url: kUrl1),
-        Text(
-          '8/19 ($kUrl2)',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        PlayerWidget(url: kUrl2),
-        Text(
-          '8/20 ($kUrl3)',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        PlayerWidget(url: kUrl3),
-        Text(
-          '8/18 (Low Latency mode) ($kUrl1)',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        PlayerWidget(url: kUrl1, mode: PlayerMode.LOW_LATENCY),
       ]),
     );
+
   }
 
   Widget localFile() {
     return _tab([
-      Text('Audio File URL: $kUrl1'),
-      _btn('Download File to your Device', () => _loadFile()),
+      Text('Audio URL: $kUrl1'),
+      _btn('Download File', () => _loadFile()),
       Text('Current local file path: $localFilePath'),
       localFilePath == null ? Container() : PlayerWidget(url: localFilePath, isLocal: true),
     ]);
-  }
-
-  Future<int> _getDuration() async {
-    File audiofile = await audioCache.load('audio2.mp3');
-    await advancedPlayer.setUrl(
-      audiofile.path,
-      isLocal: true,
-    );
-    int duration = await Future.delayed(Duration(seconds: 2), () => advancedPlayer.getDuration());
-    return duration;
-  }
-
-  getLocalFileDuration() {
-    return FutureBuilder<int>(
-      future: _getDuration(),
-      builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.none:
-            return Text('No Connection...');
-          case ConnectionState.active:
-          case ConnectionState.waiting:
-            return Text('Awaiting result...');
-          case ConnectionState.done:
-            if (snapshot.hasError) return Text('Error: ${snapshot.error}');
-            return Text('Duration is: ${Duration(milliseconds: snapshot.data)}');
-        }
-        return null; // unreachable
-      },
-    );
   }
 
   @override
@@ -141,5 +104,65 @@ class _ExampleAppState extends State<ExampleApp> {
         ),
       ),
     );
+  }
+}
+
+
+class DateTimeForm extends StatefulWidget {
+  @override
+  _DateTimeFormState createState() => _DateTimeFormState();
+}
+
+class _DateTimeFormState extends State<DateTimeForm> {
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          BasicDateField(),
+          SizedBox(height: 24),
+//          RaisedButton(
+//            child: Text('Save'),
+//            onPressed: () => formKey.currentState.save(),
+//          ),
+//          RaisedButton(
+//            child: Text('Reset'),
+//            onPressed: () => formKey.currentState.reset(),
+//          ),
+//          RaisedButton(
+//            child: Text('Validate'),
+//            onPressed: () => formKey.currentState.validate(),
+//          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BasicDateField extends StatelessWidget {
+
+  final format = DateFormat("yyyy-MM-dd");
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Align(
+        alignment: Alignment.center,
+        child: Text("Basic date field"),
+      ),
+      DateTimeField(
+        format: format,
+        onShowPicker: (context, currentValue) {
+          return showDatePicker(
+              context: context,
+              firstDate: DateTime(1900),
+              initialDate: currentValue ?? DateTime.now(),
+              lastDate: DateTime(2100));
+        },
+      ),
+    ]);
   }
 }
