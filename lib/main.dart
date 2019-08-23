d0c23bfa3572da74e6b88c829658d63043a33116
Mike Dubinsky <mike.dubinsky@odb.org>
2019-08-23 11:08:50 -0400

    Calendar selections now works (if you change tabs)

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:intl/intl.dart';
-import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'player_widget.dart';
 
 typedef void OnError(Exception exception);
@@ -15,6 +14,9 @@ typedef void OnError(Exception exception);
 const kUrl1 = 'https://dzxuyknqkmi1e.cloudfront.net/odb/2019/08/odb-08-18-19.mp3';
 const kUrl2 = 'https://dzxuyknqkmi1e.cloudfront.net/odb/2019/08/odb-08-19-19.mp3';
 const kUrl3 = 'https://dzxuyknqkmi1e.cloudfront.net/odb/2019/08/odb-08-20-19.mp3';
+
+// TODO: Bug: Need to select a date, change tabs, and return for audio player to work
+// TODO: Remove Download tab and add download button to main page
 
 void main() {
   runApp(new MaterialApp(home: new ExampleApp()));
@@ -29,10 +31,39 @@ class _ExampleAppState extends State<ExampleApp> {
   AudioCache audioCache = AudioCache();
   AudioPlayer advancedPlayer = AudioPlayer();
   String localFilePath;
-  String datePicked;
-  DateTime date;
-  TimeOfDay time;
   final dateFormat = DateFormat("MMMM-dd-yyyy");
+  DateTime selectedDate = DateTime.now();
+  String audioUrl = '';
+
+  // function to return 2 digits month or day
+  String twoDigit(String temp) {
+    if (temp.length==1) {
+      return "0" + temp;
+    } else if (temp.length==4) {
+      return temp.substring(temp.length - 2);
+    }  else {
+      return temp;
+    }
+  }
+  // Resource: https://stackoverflow.com/questions/52727535/what-is-the-correct-way-to-add-date-picker-in-flutter-app
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2010),
+        lastDate: DateTime(2030));
+    if (picked != null && picked != selectedDate) {
+      String month = twoDigit(picked.month.toString());
+      String day = twoDigit(picked.day.toString());
+      String year = twoDigit(picked.year.toString());
+      String fullYear = picked.year.toString();
+      setState(() {
+        selectedDate = picked;
+        audioUrl =
+        'https://dzxuyknqkmi1e.cloudfront.net/odb/$fullYear/$month/odb-$month-$day-$year.mp3';
+      });
+    }
+  }
 
   Future _loadFile() async {
     final bytes = await readBytes(kUrl1);
@@ -65,12 +96,20 @@ class _ExampleAppState extends State<ExampleApp> {
   Widget remoteUrl() {
     return SingleChildScrollView(
       child: _tab([
-        DateTimeForm(),
-                Text(
-         'Date: 8/18\n$kUrl1',
-          style: TextStyle(fontWeight: FontWeight.bold),
+          SizedBox(height: 20.0,),
+          RaisedButton(
+            onPressed: () => _selectDate(context),
+            child: Text('Select date'),
+          ),
+        Text(
+            "${dateFormat.format(selectedDate.toLocal())}",
+            style: TextStyle(fontWeight: FontWeight.bold)
         ),
-        PlayerWidget(url: kUrl1),
+        Text(
+            "Audio URL: $audioUrl",
+            style: TextStyle(fontWeight: FontWeight.bold)
+        ),
+        PlayerWidget(url: audioUrl),
       ]),
     );
 
@@ -104,65 +143,5 @@ class _ExampleAppState extends State<ExampleApp> {
         ),
       ),
     );
-  }
-}
-
-
-class DateTimeForm extends StatefulWidget {
-  @override
-  _DateTimeFormState createState() => _DateTimeFormState();
-}
-
-class _DateTimeFormState extends State<DateTimeForm> {
-  final formKey = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          BasicDateField(),
-          SizedBox(height: 24),
-//          RaisedButton(
-//            child: Text('Save'),
-//            onPressed: () => formKey.currentState.save(),
-//          ),
-//          RaisedButton(
-//            child: Text('Reset'),
-//            onPressed: () => formKey.currentState.reset(),
-//          ),
-//          RaisedButton(
-//            child: Text('Validate'),
-//            onPressed: () => formKey.currentState.validate(),
-//          ),
-        ],
-      ),
-    );
-  }
-}
-
-class BasicDateField extends StatelessWidget {
-
-  final format = DateFormat("yyyy-MM-dd");
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Align(
-        alignment: Alignment.center,
-        child: Text("Basic date field"),
-      ),
-      DateTimeField(
-        format: format,
-        onShowPicker: (context, currentValue) {
-          return showDatePicker(
-              context: context,
-              firstDate: DateTime(1900),
-              initialDate: currentValue ?? DateTime.now(),
-              lastDate: DateTime(2100));
-        },
-      ),
-    ]);
   }
 }
